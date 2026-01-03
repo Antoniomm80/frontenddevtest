a8fd2b506da1ceadaae584b7d4b7ebf8a574bbb9
Antonio MM <antoniomm80@gmail.com>
2026-01-03 16:00:38 +0100

    feat: implements add to cart use case

---
+++ src/modules/product/application/addToCart.js
@@ -0,0 +1,16 @@
+export class AddToCart {
+    constructor(productService, storageService) {
+        this.productService = productService;
+        this.storageService = storageService;
+    }
+
+    async execute({id, colorCode, storageCode}) {
+        try {
+            const numItems = await this.productService.addToCart({id, colorCode, storageCode});
+            await this.storageService.saveCartNumItems(numItems);
+        } catch (error) {
+            console.error('Use case error - AddToCart:', error);
+            throw error;
+        }
+    }
+}
